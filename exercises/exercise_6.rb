aa5dbccc68a6ad7ec2a0c2313b54541979f66a80
Kevin Convery <kevinconvery@gmail.com>
2019-10-14 20:57:30 +0000

    exercises 6 and 7 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,6 +5,13 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Juan", last_name: "Gonzalez", hourly_rate: 45)
+@store1.employees.create(first_name: "Vasiliy", last_name: "Klimkin", hourly_rate: 42)
+@store2.employees.create(first_name: "Emma", last_name: "MacPhail", hourly_rate: 65)
+@store2.employees.create(first_name: "Janelle", last_name: "Stiehl", hourly_rate: 55)
+@store2.employees.create(first_name: "Johnny", last_name: "Li", hourly_rate: 47)
+
 puts "Exercise 6"
 puts "----------"
 
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -8,5 +8,13 @@ require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
+print "What would you like to call the new store? "
+new_store = Store.new(name: gets.chomp)
+puts new_store.valid?
+puts new_store.errors.size
+puts new_store.errors.to_a
+# new_store.errors.each do |error|
+#   puts error
+# end
 
 # Your code goes here ...
