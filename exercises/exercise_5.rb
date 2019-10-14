238c9b0a13162891524668b679b4d7c4fa56f1be
Kevin Convery <kevinconvery@gmail.com>
2019-10-14 20:06:45 +0000

    exercise 5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,12 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+
 puts "Exercise 5"
 puts "----------"
+puts "Total revenue from all stores: #{total_revenue}"
+puts "Average revenue per store: #{average_revenue}"
 
 # Your code goes here ...
