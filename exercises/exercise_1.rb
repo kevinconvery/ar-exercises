21d7d2881def1c9e3a7fff8b4bff5bef917a6992
Kevin Convery <kevinconvery@gmail.com>
2019-10-14 19:49:30 +0000

    exercise 3 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,7 +1,6 @@
 require_relative '../setup'
 
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-
 # Richmond (annual_revenue of 1260000 carries women's apparel only)
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 # Gastown (annual_revenue of 190000 carries men's apparel only)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,6 +3,14 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+
 puts "Exercise 4"
 puts "----------"
 
